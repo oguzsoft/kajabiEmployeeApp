769eac861a4940f46b81513b0790539fda2afca2
oguzsoft <oguzkaangunduz@gmail.com>
2022-12-08 23:22:32 +0300

    Add Vue and App.vue

---
+++ app/javascript/components/App.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1 class="text-3xl font-bold underline">Hello vue</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
