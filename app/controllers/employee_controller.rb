7938d735fd2c74abb349ad5e86a5961c36912cfa
oguzsoft <oguzkaangunduz@gmail.com>
2022-12-16 23:37:51 +0300

    Add controller methods

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,19 +1,26 @@
 class EmployeeController < ApplicationController
   before_action :employeeService
-
   def index
-
   end
 
   def getEmps
-    @employees = employeeService.getAllUsers
+    @employees = employeeService.getAllUsers(params[:id])
     render json: @employees
+  end
+
+  def searchedData
+    @employees = employeeService.searchedData(params[:parameter])
+    render json: @employees
+  end
+
+  def totalPages
+    employeeService.totalPage
   end
 
   def show
     @employee = employeeService.getUser(params[:id])
+    @employee = JSON.parse!(@employee)
   end
-
 
   def employeeService
     Employee.new
