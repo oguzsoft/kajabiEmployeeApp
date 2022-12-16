a552c9fdba221c2dbfe0183e46dfb15a85388a65
oguzsoft <oguzkaangunduz@gmail.com>
2022-12-16 23:39:42 +0300

    Update  routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   root to: 'employee#index'
   get 'employee/index'
   get 'employee/show'
-  get 'employee/getEmps'
+  get 'employee/getEmps/:id', to: 'employee#getEmps'
+  get 'employee/getUserInformation/:id', to: 'employee#getUserInformation'
+  get 'employee/searchedData/:parameter', to: 'employee#searchedData'
+  get 'employee/totalPages', to: 'employee#totalPages'
   get '/employee/:id', to: 'employee#show'
-  #resources :employee
+  resources :employee
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
